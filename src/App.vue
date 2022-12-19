95522a65b25c29d731e6c2b43244ede0ff5c513f
dev-charles15531 <kingcharles15531@gmail.com>
2022-12-19 11:18:02 +0100

    vue skeleton project + tailwind css

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Calendar from "./components/Calendar.vue";
+</script>
+
+<template>
+  <section>
+    <h3 class="text-lg font-extrabold text-green-400">Hello Vue + tailwind</h3>
+  </section>
+
+  <Calendar />
+</template>
---
+++ src/components/Calendar.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class=""></div>
+</template>
+
+<script setup></script>
